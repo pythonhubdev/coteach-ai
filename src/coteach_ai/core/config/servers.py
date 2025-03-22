050e0e8b2ea42343ca70b50fa33f4c1b34108088
Vetrichelvan <pythonhubdev@gmail.com>
2025-03-22 22:23:23 +0530

    feat: Add configuration and settings for development environment, including logging and security settings

---
+++ src/coteach_ai/core/config/servers.py
@@ -0,0 +1,26 @@
+from litestar.openapi.spec import Server
+
+from .base import settings
+
+if settings.app.DEBUG:
+	server_config = [
+		Server(
+			url="http://0.0.0.0:8000",
+			description="Development server (0.0.0.0)",
+		),
+		Server(
+			url="http://127.0.0.1:8000",
+			description="Development server (localhost)",
+		),
+	]
+else:
+	server_config = [
+		Server(
+			url="https://prodapi.panorah.com",
+			description="Production server",
+		),
+		Server(
+			url="http://127.0.0.1:8000",
+			description="Development server (localhost)",
+		),
+	]
