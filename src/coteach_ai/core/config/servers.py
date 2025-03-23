70a1ea32188b1d95b0e4d5240f76522b5358d4db
Vetrichelvan <pythonhubdev@gmail.com>
2025-03-23 11:48:14 +0530

    feat: Add LoggingMiddleware for enhanced request logging and update dependency specifications

--- src/coteach_ai/core/config/servers.py
+++ src/coteach_ai/core/config/servers.py
@@ -1,26 +1,12 @@
 from litestar.openapi.spec import Server
 
-from .base import settings
-
-if settings.app.DEBUG:
-	server_config = [
+server_config = [
 		Server(
-			url="http://0.0.0.0:8000",
-			description="Development server (0.0.0.0)",
+				url="https://api.coteach.com",
+				description="Production server",
 		),
 		Server(
-			url="http://127.0.0.1:8000",
-			description="Development server (localhost)",
+				url="http://127.0.0.1:8000",
+				description="Development server (localhost)",
 		),
-	]
-else:
-	server_config = [
-		Server(
-			url="https://api.coteach.com",
-			description="Production server",
-		),
-		Server(
-			url="http://127.0.0.1:8000",
-			description="Development server (localhost)",
-		),
-	]
+]
---
+++ src/coteach_ai/middlewares/logging_middleware.py
@@ -0,0 +1,40 @@
+from time import time
+from typing import TYPE_CHECKING
+
+from coteach_ai.core.utils.logging import end_stage_logger, stage_logger
+from litestar.datastructures import MutableScopeHeaders
+from litestar.enums import ScopeType
+from litestar.middleware import AbstractMiddleware
+from litestar.types import Receive, Scope, Scopes, Send
+
+if TYPE_CHECKING:
+	from litestar.types import Message, Receive, Scope, Send
+
+
+class LoggingMiddleware(AbstractMiddleware):
+	scopes: Scopes = {ScopeType.HTTP}  # noqa: RUF012
+
+	async def __call__(self, scope: "Scope", receive: "Receive", send: "Send") -> None:
+		start_time = time()
+
+		if scope["type"] == ScopeType.HTTP and scope.get("client"):
+			stage_logger.info(
+					f'{scope["client"][0]}:{scope["client"][1]} - "{scope["method"]} {scope["path"]} '  # type: ignore
+					f'{scope["http_version"]}"',
+			)
+
+		async def send_wrapper(message: "Message") -> None:
+			if message["type"] == "http.response.start":
+				process_time = time() - start_time
+				headers = MutableScopeHeaders.from_message(message=message)
+				headers["X-Process-Time"] = str(process_time)
+
+				if scope.get("client"):
+					end_stage_logger.info(
+							f'{scope["client"][0]}:{scope["client"][1]} - "{scope["method"]} '  # type: ignore
+							f'{scope["path"]} '  # type: ignore
+							f'{scope["http_version"]}" {message["status"]}',
+					)
+			await send(message)
+
+		await self.app(scope, receive, send_wrapper)
